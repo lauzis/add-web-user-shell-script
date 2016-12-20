4b206b31a8b749a9d3b35653d7c146a6d8f06755
Aivars Lauzis <lauzis@inbox.lv>
2016-12-20 19:19:39 +0200

    inital commit

---
+++ add_web_user.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# author: Aivars Lauzis
+# email: lauzis@inbox.lv
+
+#todo check if all necassary toolas are there
+#todo do we have openssl
+#todo do we have mkpasswd
+#todo get ussername as passed param
+#todo try to install ncassary packages
+#todo install sudo apt-get install whois
+#todo install sudo apt-get install openssl
+$todo check if script is in sudo mode
+
+#setting the username and php user name
+NEW_USER_NAME="dev_uwp"
+PHP_USER_NAME="php_"$NEW_USER_NAME;
+
+#should check if such user
+useradd $NEW_USER_NAME
+
+#generates new password
+NEW_USER_PASS="$(openssl rand -base64 20)"
+#hashes password for updating the user
+NEW_USER_PASS_HASH="$(mkpasswd -m sha-256 $NEW_USER_PASS)"
+
+#changes user passowrd
+usermod -p $NEW_USER_PASS_HASH $NEW_USER_NAME
+
+#create home dirs
+mkdir /home/$NEW_USER_NAME
+mkdir /home/$NEW_USER_NAME/logs
+mkdir /home/$NEW_USER_NAME/www
+
+#changes the ownership of the directories
+chown $NEW_USER_NAME /home/$NEW_USER_NAME -R
+chgrp $NEW_USER_NAME /home/$NEW_USER_NAME -R
+chgrp www-data /home/$NEW_USER_NAME/logs -R
+
+#creates php user, wihtout password
+groupadd $PHP_USER_NAMER
+sudo useradd -g php_$NEW_USER_NAME php_$NEW_USER_NAME
+
+#todo copy standart php.conf
+#todo copy standart nginx.conf
+
+
+
+
+echo $NEW_USER_PASS
